46eca44f639304edce318b0214aa67f58608f792
Daniel van der Ende <daniel.vanderende@gmail.com>
2020-10-16 15:29:30 +0200

    Add dag showing azure blob sensor

---
+++ dags/azure_blob_sensor/dag.py
@@ -0,0 +1,21 @@
+import airflow.utils.dates
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+
+from airflow.contrib.sensors.wasb_sensor import WasbBlobSensor
+
+dag = DAG(dag_id="azure_blob_sensor", start_date=airflow.utils.dates.days_ago(3), schedule_interval="@daily")
+
+
+def do_magic(**context):
+    print(context)
+
+
+data_arrival_sensor = WasbBlobSensor(
+   container_name="landing_zone",
+   blob_name="raw_data.csv"
+)
+
+data_has_arrived = BashOperator(task_id="data_has_arrived", bash_command="echo 'The data has arrived!'", dag=dag)
+
+data_arrival_sensor >> data_has_arrived
