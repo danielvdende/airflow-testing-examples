cfef9bf66ec2501e005d0f55ba495a898724a575
Daniel van der Ende <daniel.vanderende@gmail.com>
2020-10-23 14:55:07 +0200

    Add prefix sensor

--- dags/azure_blob_sensor/dag.py
+++ dags/azure_blob_sensor/dag.py
@@ -2,22 +2,29 @@ import airflow.utils.dates
 from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
 
-from airflow.contrib.sensors.wasb_sensor import WasbBlobSensor
+from airflow.contrib.sensors.wasb_sensor import WasbBlobSensor, WasbPrefixSensor
 
 dag = DAG(dag_id="azure_blob_sensor", start_date=airflow.utils.dates.days_ago(3), schedule_interval="@daily")
-
-
-def do_magic(**context):
-    print(context)
 
 
 data_arrival_sensor = WasbBlobSensor(
     task_id="data_arrival_sensor",
     container_name="landing",
     blob_name="raw_data.csv",
-    wasb_conn_id="landing_conn"
+    wasb_conn_id="landing_conn",
+    poke_interval=60,
+    timeout=60*60*24
 )
 
-data_has_arrived = BashOperator(task_id="data_has_arrived", bash_command="echo 'The data has arrived!' && sleep 5m", dag=dag)
+data_file_prefix_sensor = WasbPrefixSensor(
+    task_id="data_file_prefix_sensor",
+    container_name="landing",
+    prefix="raw_",
+    wasb_conn_id="landing_conn",
+    poke_interval=60,
+    timeout=60*60*24
+)
 
-data_arrival_sensor >> data_has_arrived
+data_has_arrived = BashOperator(task_id="data_has_arrived", bash_command="echo 'The data has arrived!'", dag=dag)
+
+[data_arrival_sensor, data_file_prefix_sensor] >> data_has_arrived
