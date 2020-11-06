6824a1ae2d8b99a65b9c0b9819eb2cc82a483eb6
Daniel van der Ende <daniel.vanderende@gmail.com>
2020-11-06 11:55:48 +0100

    Try to get it working

--- dags/datafactory/dag.py
+++ dags/datafactory/dag.py
@@ -10,9 +10,12 @@ sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 from datafactory.trigger_data_factory_run_operator import TriggerDataFactoryRunOperator
 
 dag = DAG(dag_id="datafactory_integration", start_date=airflow.utils.dates.days_ago(3), schedule_interval="@daily")
+subscription_id="5ddf05c0-b972-44ca-b90a-3e49b5de80dd"
 
 trigger_data_factory_run = TriggerDataFactoryRunOperator(task_id="trigger_data_factory_run",
                                                          dag=dag,
+                                                         subscription_id=subscription_id,
+                                                         adf_sp_connection_id="azure_data_factory",
                                                          pipeline_name="copyPipeline",
                                                          resource_group_name="abnairflowazure-20631bd75d7412",
                                                          factory_name="adf20631bd75d")
