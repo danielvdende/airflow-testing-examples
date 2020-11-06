b262e65b7c05added91cc34ca7d5593eb60b68ab
Daniel van der Ende <daniel.vanderende@gmail.com>
2020-11-06 11:50:52 +0100

    Use Tim's operator/hook pair
    These are much more concise than the lumpy things found online

--- dags/datafactory/trigger_data_factory_run_operator.py
+++ dags/datafactory/trigger_data_factory_run_operator.py
@@ -1,82 +1,98 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-#
 import time
-from typing import Any
-
+from typing import Optional, Dict, Any
+from airflow import AirflowException
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from msrestazure.azure_exceptions import CloudError
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from data_factory.data_factory_hook import AzureDataFactoryHook
 
-from datafactory.data_factory_hook import AzureDataFactoryHook
+BASE_RUN_URL = (
+    "https://adf.azure.com/monitoring"
+    "/pipelineruns/{run_id}?factory="
+    "/subscriptions/{subscription_id}"
+    "/resourceGroups/{resource_group}"
+    "/providers/Microsoft.DataFactory/factories/{factory_name}"
+
+)
 
 
 class TriggerDataFactoryRunOperator(BaseOperator):
-    """
-    Deletes blob(s) on Azure Blob Storage.
-    :param container_name: Name of the container. (templated)
-    :type container_name: str
-    :param blob_name: Name of the blob. (templated)
-    :type blob_name: str
-    :param wasb_conn_id: Reference to the wasb connection.
-    :type wasb_conn_id: str
-    :param check_options: Optional keyword arguments that
-        `WasbHook.check_for_blob()` takes.
-    :param is_prefix: If blob_name is a prefix, delete all files matching prefix.
-    :type is_prefix: bool
-    :param ignore_if_missing: if True, then return success even if the
-        blob does not exist.
-    :type ignore_if_missing: bool
-    """
+    template_fields = [
+        "resource_group_name",
+        "factory_name",
+        "pipeline_name",
+        "pipeline_parameters",
+    ]
+    template_ext = ()
+    ui_color = "#008ad7"
 
     @apply_defaults
     def __init__(
             self,
-            *,
-            pipeline_name: str = "",
-            factory_name: str = "",
-            resource_group_name: str = "",
-            azure_conn_id: str = 'azure_default',
+            adf_sp_connection_id: str,
+            subscription_id: str,
+            resource_group_name: str,
+            factory_name: str,
+            pipeline_name: str,
+            pipeline_parameters: Optional[Dict[str, Any]],
+            polling_period_seconds: 300,
+            *args,
             **kwargs,
-    ) -> None:
-        super().__init__(**kwargs)
-        self.azure_conn_id = azure_conn_id
-        self.pipeline_name = pipeline_name
-        self.factory_name = factory_name
+    ):
+        super().__init__(*args, **kwargs)
+        self.adf_sp_connection_id = adf_sp_connection_id
+        self.subscription_id = subscription_id
         self.resource_group_name = resource_group_name
+        self.factory_name = factory_name
+        self.pipeline_name = pipeline_name
+        self.pipeline_parameters = pipeline_parameters or {}
+        self.polling_period_seconds = polling_period_seconds
 
-    def execute(self, context: dict) -> None:
-        self.log.info(f"Running pipeline {self.pipeline_name} in factory {self.factory_name}")
-        hook = AzureDataFactoryHook(conn_id=self.azure_conn_id)
+    def execute(self, context):
+        hook = AzureDataFactoryHook(self.adf_sp_connection_id)
+        try:
+            response = hook.client.pipelines.create_run(
+                resource_group_name=self.resource_group_name,
+                factory_name=self.factory_name,
+                pipeline_name=self.pipeline_name,
+                parameters=self.pipeline_parameters,
+            )
+            self.log.info(
+                "ADF Pipeline run for pipeline %s has been created with run id %s",
+                self.pipeline_name,
+                response.run_id,
+            )
+            self.handle_adf_run(response.run_id, hook)
+        except CloudError as e:
+            raise AirflowException(e)
 
-
-        run_response = hook.run_pipeline(self.pipeline_name, resource_group_name=self.resource_group_name, factory_name=self.factory_name)
-
-        # Monitor the pipeline run
-        status = hook.get_pipeline_run(run_id=run_response.run_id, resource_group_name=self.resource_group_name, factory_name=self.factory_name).status
-        self.log.info(status)
-        while status == "InProgress" or status == "Queued":
-            time.sleep(1)
-            status = hook.get_pipeline_run(run_id=run_response.run_id, resource_group_name=self.resource_group_name, factory_name=self.factory_name).status
-            self.log.info("pipeline still running")
-            self.log.info(status)
-
-        status = hook.get_pipeline_run(run_id=run_response.run_id, resource_group_name=self.resource_group_name, factory_name=self.factory_name).status
-        self.log.info(status)
-        if status != "Succeeded":
-            self.log.info("FAILFAILFAIL")
-            exit(1)
+    def handle_adf_run(self, run_id: str, hook: AzureDataFactoryHook):
+        see_logs = "View run status and logs at {logs}".format(
+            logs=BASE_RUN_URL.format(
+                run_id=run_id,
+                subscription_id=self.subscription_id,
+                resource_group=self.resource_group_name,
+                factory_name=self.factory_name,
+            )
+        )
+        while True:
+            run_state = hook.client.pipeline_runs.get(
+                self.resource_group_name, self.factory_name, run_id
+            )
+            if run_state.status == "Succeeded":
+                self.log.info(f"Run with id {run_id} completed successfully.",)
+                self.log.info(see_logs)
+                return
+            elif run_state.status in ["Queued", "InProgress"]:
+                self.log.info(f"Run with id {run_id} {run_state.status}...",)
+                time.sleep(self.polling_period_seconds)
+                continue
+            else:
+                error_message = "{task} failed with terminal state {state} and message {message}".format(
+                    task=self.task_id, state=run_state.status, message=run_state.message
+                )
+                self.log.info(see_logs)
+                raise AirflowException(error_message)
