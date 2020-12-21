f15cfbe4b89acd0a58cbe864f4d6cbac831a56fc
Daniel van der Ende <daniel.vanderende@gmail.com>
2020-12-21 14:40:28 +0100

    Cleanup

--- dags/adls/azure_blob_to_adls_operator.py
+++ dags/adls/azure_blob_to_adls_operator.py
@@ -1,5 +1,4 @@
 from tempfile import NamedTemporaryFile
-from typing import Sequence
 
 from airflow.contrib.hooks.wasb_hook import WasbHook
 from airflow.contrib.hooks.azure_data_lake_hook import AzureDataLakeHook
@@ -8,74 +7,6 @@ from airflow.utils.decorators import apply_defaults
 
 
 class AzureBlobToADLSOperator(BaseOperator):
-    """
-    Synchronizes an Azure Data Lake Storage path with a GCS bucket
-    :param src_adls: The Azure Data Lake path to find the objects (templated)
-    :type src_adls: str
-    :param dest_gcs: The Google Cloud Storage bucket and prefix to
-        store the objects. (templated)
-    :type dest_gcs: str
-    :param replace: If true, replaces same-named files in GCS
-    :type replace: bool
-    :param gzip: Option to compress file for upload
-    :type gzip: bool
-    :param azure_data_lake_conn_id: The connection ID to use when
-        connecting to Azure Data Lake Storage.
-    :type azure_data_lake_conn_id: str
-    :param gcp_conn_id: (Optional) The connection ID used to connect to Google Cloud.
-    :type gcp_conn_id: str
-    :param google_cloud_storage_conn_id: (Deprecated) The connection ID used to connect to Google Cloud.
-        This parameter has been deprecated. You should pass the gcp_conn_id parameter instead.
-    :type google_cloud_storage_conn_id: str
-    :param delegate_to: Google account to impersonate using domain-wide delegation of authority,
-        if any. For this to work, the service account making the request must have
-        domain-wide delegation enabled.
-    :type delegate_to: str
-    :param google_impersonation_chain: Optional Google service account to impersonate using
-        short-term credentials, or chained list of accounts required to get the access_token
-        of the last account in the list, which will be impersonated in the request.
-        If set as a string, the account must grant the originating account
-        the Service Account Token Creator IAM role.
-        If set as a sequence, the identities from the list must grant
-        Service Account Token Creator IAM role to the directly preceding identity, with first
-        account from the list granting this role to the originating account (templated).
-    :type google_impersonation_chain: Union[str, Sequence[str]]
-    **Examples**:
-        The following Operator would copy a single file named
-        ``hello/world.avro`` from ADLS to the GCS bucket ``mybucket``. Its full
-        resulting gcs path will be ``gs://mybucket/hello/world.avro`` ::
-            copy_single_file = AdlsToGoogleCloudStorageOperator(
-                task_id='copy_single_file',
-                src_adls='hello/world.avro',
-                dest_gcs='gs://mybucket',
-                replace=False,
-                azure_data_lake_conn_id='azure_data_lake_default',
-                gcp_conn_id='google_cloud_default'
-            )
-        The following Operator would copy all parquet files from ADLS
-        to the GCS bucket ``mybucket``. ::
-            copy_all_files = AdlsToGoogleCloudStorageOperator(
-                task_id='copy_all_files',
-                src_adls='*.parquet',
-                dest_gcs='gs://mybucket',
-                replace=False,
-                azure_data_lake_conn_id='azure_data_lake_default',
-                gcp_conn_id='google_cloud_default'
-            )
-         The following Operator would copy all parquet files from ADLS
-         path ``/hello/world``to the GCS bucket ``mybucket``. ::
-            copy_world_files = AdlsToGoogleCloudStorageOperator(
-                task_id='copy_world_files',
-                src_adls='hello/world/*.parquet',
-                dest_gcs='gs://mybucket',
-                replace=False,
-                azure_data_lake_conn_id='azure_data_lake_default',
-                gcp_conn_id='google_cloud_default'
-            )
-    """
-
-    ui_color = '#f0eee4'
-
     @apply_defaults
     def __init__(
             self,
@@ -119,4 +50,4 @@ class AzureBlobToADLSOperator(BaseOperator):
                 adls_hook = WasbHook(wasb_conn_id=self.azure_data_lake_conn_id)
                 adls_hook.load_file(f.name, container_name=self.dest_adls_container, blob_name=self.dest_adls)
 
-        self.log.info("All done, uploaded files to Azure Blob")
+        self.log.info("All done, uploaded files to Azure Data Lake Store")
